0f4ba205b741d418f8ac3e0f36bf2c5ca175ac88
Fadi <f.eidkatrina@student.maastrichtuniversity.nl>
2022-06-16 19:06:07 +0200

    trying in the function call

--- wsgi.py
+++ wsgi.py
@@ -6,12 +6,12 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
-
-
-if __name__ == "__main__":
     import os
     cwd = os.getcwd()
     print(cwd)
     start_experiments()
+    return "Hello World!"
+
+
+if __name__ == "__main__":
     application.run()
