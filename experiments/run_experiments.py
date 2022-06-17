e8ea5481909ff8216de40ddee5d74f541e529d96
Fadi <f.eidkatrina@student.maastrichtuniversity.nl>
2022-06-17 12:46:35 +0200

    defaults for certain run_experiments.py if not specified in json input

--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -10,19 +10,21 @@ def start_experiments(experiments=None):
 
 	for experiment in experiments:
 		for run_no in range(experiment["number_of_runs"]):
-			print(f"Now running {run_no} of {experiment['experiment_name']}")
+			exp_name = experiment['experiment_name']
+			print(f"Now running {run_no} of {exp_name}")
 			config = experiment["config"]
 			config["algo_config"]["image_filename"] = f"{experiment['experiment_name']}_{run_no}"
 			Main(
-				algorithm_class=config["algorithm_class"],
-				trip_requests_filename=config["trip_requests_filename"],
-				log_filename=f"{config[f'log_filename']}_{run_no}",
-				output_results_filename=f"{config[f'output_results_filename']}_{run_no}",
-				check_results_filename=config["check_results_filename"],
-				general_config_filename=config["general_config_filename"],
-				car_charge_config_filename=config["car_charge_config_filename"],
-				station_metrics_filename=config["station_metrics_filename"],
-				algo_config=config["algo_config"]
+				algorithm_class=config.get("algorithm_class"),
+				trip_requests_filename=config.get("trip_requests_filename"),
+				log_filename=f"{config.get('log_filename', exp_name)}_{run_no}",
+				output_results_filename=f"{config.get('output_results_filename', exp_name)}_{run_no}",
+				check_results_filename=config.get("check_results_filename", 'nothing'),
+				general_config_filename=config.get("general_config_filename", 'default'),
+				car_charge_config_filename=config.get("car_charge_config_filename", 'default'),
+				station_metrics_filename=config.get("station_metrics_filename"),
+				algo_config=config.get("algo_config"),
+				assign_cars_only_after_all_trips_announced=config.get("assign_cars_only_after_all_trips_announced", False)
 			)
 
 
