d8a3f3e0601ee5577dbe4e5688035d1d040c6dfe
Fadi <f.eidkatrina@student.maastrichtuniversity.nl>
2022-06-17 12:56:01 +0200

    parallelized run_experiments.py

--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -1,5 +1,6 @@
 from source.main import Main
 import json
+from joblib import Parallel, delayed
 
 
 def start_experiments(experiments=None):
@@ -8,24 +9,27 @@ def start_experiments(experiments=None):
 		f = open(f'./experiments/experiments.json')
 		experiments = json.load(f)
 
-	for experiment in experiments:
-		for run_no in range(experiment["number_of_runs"]):
-			exp_name = experiment['experiment_name']
-			print(f"Now running {run_no} of {exp_name}")
-			config = experiment["config"]
-			config["algo_config"]["image_filename"] = f"{experiment['experiment_name']}_{run_no}"
-			Main(
-				algorithm_class=config.get("algorithm_class"),
-				trip_requests_filename=config.get("trip_requests_filename"),
-				log_filename=f"{config.get('log_filename', exp_name)}_{run_no}",
-				output_results_filename=f"{config.get('output_results_filename', exp_name)}_{run_no}",
-				check_results_filename=config.get("check_results_filename", 'nothing'),
-				general_config_filename=config.get("general_config_filename", 'default'),
-				car_charge_config_filename=config.get("car_charge_config_filename", 'default'),
-				station_metrics_filename=config.get("station_metrics_filename"),
-				algo_config=config.get("algo_config"),
-				assign_cars_only_after_all_trips_announced=config.get("assign_cars_only_after_all_trips_announced", False)
-			)
+	Parallel(n_jobs=10)(delayed(run_experiment)(exp) for exp in experiments)
+
+
+def run_experiment(experiment):
+	for run_no in range(experiment["number_of_runs"]):
+		exp_name = experiment['experiment_name']
+		print(f"Now running {run_no} of {exp_name}")
+		config = experiment["config"]
+		config["algo_config"]["image_filename"] = f"{experiment['experiment_name']}_{run_no}"
+		Main(
+			algorithm_class=config.get("algorithm_class"),
+			trip_requests_filename=config.get("trip_requests_filename"),
+			log_filename=f"{config.get('log_filename', exp_name)}_{run_no}",
+			output_results_filename=f"{config.get('output_results_filename', exp_name)}_{run_no}",
+			check_results_filename=config.get("check_results_filename", 'nothing'),
+			general_config_filename=config.get("general_config_filename", 'default'),
+			car_charge_config_filename=config.get("car_charge_config_filename", 'default'),
+			station_metrics_filename=config.get("station_metrics_filename"),
+			algo_config=config.get("algo_config"),
+			assign_cars_only_after_all_trips_announced=config.get("assign_cars_only_after_all_trips_announced", False)
+		)
 
 
 if __name__ == "__main__":
