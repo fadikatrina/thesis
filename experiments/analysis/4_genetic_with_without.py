238182985851faf24770ba8abeb9f587a554930d
Fadi <f.eidkatrina@student.maastrichtuniversity.nl>
2022-07-31 18:04:48 +0200

    Revert "attempt to fix the genetic rare bug"
    This reverts commit a7f847c1

---
+++ experiments/analysis/4_genetic_with_without.py
@@ -0,0 +1,69 @@
+import logging
+from pathlib import Path
+from os import walk
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+OUTPUT_FOLDER_NAME = 'genetic_with_without'
+
+
+def run():
+	result_paths = ['../results/genetic_dynamic/output/results', '../results/genetic_dynamic_compact/output/results', '../results/genetic_not_dynamic/output/results', '../results/genetic_not_dynamic_compact/output/results']
+
+	dict_res = {"notdynamic_with": 0}
+
+	for result_path in result_paths:
+
+		filenames = next(walk(result_path), (None, None, []))[2]
+		type_d = "dynamic"
+		if "not" in result_path:
+			type_d = "notdynamic"
+		if "compact" in result_path:
+			type_d += "_compact"
+
+		for i in range(2):
+			if i == 0:
+				filenames_filtered = [k for k in filenames if "out" in k]
+				type = type_d + "_without"
+			else:
+				filenames_filtered = [k for k in filenames if "out" not in k]
+				type = type_d + "_with"
+
+			perc_current = []
+			for filename in filenames_filtered:
+
+				with open(f"{result_path}/{filename}") as f:
+					lines = f.readlines()
+					perc_current.append(float(lines[4].split(" ")[3]))
+			if len(perc_current) == 0:
+				logging.warning(f"{result_path} {type} NO RESULTS, SO SKIPPIES")
+				continue
+
+			dict_res[type] = sum(perc_current) / len(perc_current)
+
+	for dynamic in ["dynamic", "notdynamic"]:
+
+		x_labels = ['1 Day', '1 Hour']
+		x_axis = np.arange(len(x_labels))
+		plt.figure(200)
+
+		plt.bar(x_axis - 0.1, [dict_res[f"{dynamic}_with"], dict_res[f"{dynamic}_compact_with"]], width=0.2, label='With Genetic')
+		plt.bar(x_axis + 0.1, [dict_res[f"{dynamic}_without"], dict_res[f"{dynamic}_compact_without"]], width=0.2, label='Without Genetic')
+
+		plt.xticks(x_axis, x_labels)
+
+		plt.legend()
+		plt.title(f"{dynamic}")
+		plt.xlabel('Sim Duration')
+		plt.ylabel('Perc Completed')
+		plt.ylim(ymin=0)
+		plt.savefig(f'./viz/{OUTPUT_FOLDER_NAME}/{dynamic}.jpg')
+		plt.show()
+
+
+if __name__ == "__main__":
+	Path(f"./logs").mkdir(parents=True, exist_ok=True)
+	Path(f"./viz/{OUTPUT_FOLDER_NAME}").mkdir(parents=True, exist_ok=True)
+	logging.basicConfig(filename=f"./logs/{OUTPUT_FOLDER_NAME}.log", level=logging.DEBUG, format='%(message)s')
+	run()
